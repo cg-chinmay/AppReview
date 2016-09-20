6a2f85e21788aa4bcc41f4e3c12cbed34f2f07ea
Chinmay Gadgil <mailcg92@gmail.com>
2016-09-20 21:03:48 +0530

    Changed Default App to Rakuma

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Apple provides App reviews as xml file. The above tool lists them in a convenien
 Low rating reviews are highlighted . <br/>
 
 #How To View
-The sample default application used is Pokemon Go US version . <br/>
+The sample default application used is Rakuma. <br/>
 Change the iTUnes URL and hit Get Reviews button to update .
 Try It : https://rawgit.com/cg-chinmay/AppReview/master/review.html
 
--- fetchReviews.js
+++ fetchReviews.js
@@ -1,7 +1,7 @@
 //var review_url = "https://itunes.apple.com/jp/rss/customerreviews/page=2/id=928453590/sortby=mostrecent/xml?urlDesc=/customerreviews/id=928453590/sortBy=mostRecent/xml";
-var defaultAppUrl = "https://itunes.apple.com/us/app/pokemon-go/id1094591345?mt=8&ign-mpt=uo%3D2"
+var defaultAppUrl = "https://itunes.apple.com/jp/app/furimaapuri-rakuma-chu-pin/id928453590?mt=8"
 var app = "";
-var country = "us";
+var country = "jp";
 $(function() {
    $("#appUrl").val(defaultAppUrl);
   
